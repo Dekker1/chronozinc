9ee0e693d1937afcc7645d00808d77215d78e38c
Jip J. Dekker <jip@dekker.li>
2016-11-24 23:35:22 +0100

    Add cobra to manage CLI

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "chonozinc [description]",
+	Short: "Test and time your minizinc models!",
+	Long: `ChonoZinc is a small tool that wraps around mzn-fzn. It main purpose is to
+	test MiniZinc models using different solvers. It run your your model on all or
+	a sub-set of declared solvers and report on the statistics gathered. The data
+	gathered is saved in every stage.`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(-1)
+	}
 }
